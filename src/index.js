dbd68e493e9e92395e009aa36df4f99494d3121f
pate <jojomayertuff@yandex.ru>
2018-10-06 13:44:13 +0300

    add playing track

--- src/index.js
+++ src/index.js
@@ -8,40 +8,40 @@ import './index.css';
 //Hi-hat		http://d.zaix.ru/8UoU.mp3
 //Crash		http://d.zaix.ru/8UoV.mp3
 
-var drums = new Tone.Players({
-	"Kick": "http://d.zaix.ru/8UoS.mp3",
-	"Snare": "http://d.zaix.ru/8UoT.mp3",
-	"HiHat": "http://d.zaix.ru/8UoU.mp3"
-}).toMaster();
-
-var loop = new Tone.Sequence(function(time, drum) {
-	drums.get(drum).start(time, 0, "8n", 0);
-}, [["Kick", "Kick"], "Snare"], "4n");
-
-var hiHatloop = new Tone.Sequence(function(time, drum) {
-	drums.get(drum).start(time, 0, "4n", 0);
-}, ["HiHat"], "8n");
-
-function play() {
-	console.log('play');
-	loop.start();
-	hiHatloop.start();
-}
-
-function stop() {
-	console.log('stop');
-	loop.stop();
-	hiHatloop.stop();
-}
-
-Tone.Transport.bpm.value = 68;
-Tone.Transport.start();
-
-var buttPlay = document.getElementById('play');
-buttPlay.onclick = play;
-
-var buttStop = document.getElementById('stop');
-buttStop.onclick = stop;
+// var drums = new Tone.Players({
+// 	"Kick": "http://d.zaix.ru/8UoS.mp3",
+// 	"Snare": "http://d.zaix.ru/8UoT.mp3",
+// 	"HiHat": "http://d.zaix.ru/8UoU.mp3"
+// }).toMaster();
+//
+// var loop = new Tone.Sequence(function(time, drum) {
+// 	drums.get(drum).start(time, 0, "8n", 0);
+// }, [["Kick", "Kick"], "Snare"], "4n");
+//
+// var hiHatloop = new Tone.Sequence(function(time, drum) {
+// 	drums.get(drum).start(time, 0, "4n", 0);
+// }, ["HiHat"], "8n");
+//
+// function play() {
+// 	console.log('play');
+// 	loop.start();
+// 	hiHatloop.start();
+// }
+//
+// function stop() {
+// 	console.log('stop');
+// 	loop.stop();
+// 	hiHatloop.stop();
+// }
+//
+// Tone.Transport.bpm.value = 68;
+// Tone.Transport.start();
+//
+// var buttPlay = document.getElementById('play');
+// buttPlay.onclick = play;
+//
+// var buttStop = document.getElementById('stop');
+// buttStop.onclick = stop;
 
 class App extends React.Component {
 
@@ -56,6 +56,10 @@ class App extends React.Component {
 
 class Sequencer extends React.Component {
 
+	play() {
+		console.log('playin music');
+	}
+
 	render() {
 		return (
 			<div className="sequencer">
@@ -68,7 +72,8 @@ class Sequencer extends React.Component {
 					<TrackNames />
 					<TrackNotes />
 				</div>
-				<SequencerButtons />
+				<SequencerButtons
+				play = {this.play}/>
 			</div>
 		);
 	}
@@ -141,25 +146,55 @@ class Playhead extends React.Component {
 }
 
 class Track extends React.Component {
+
+	constructor() {
+		super();
+		this.kickPlayer = new Tone.Player(
+			"http://d.zaix.ru/8UoS.mp3",
+			function() {this.start}
+		).toMaster();
+		this.notesToPlayArr = [];
+		for (var i = 0; i < 16; i ++) {
+			this.notesToPlayArr.push(false);
+		}
+	}
+
+	playTrack() {
+		Tone.Transport.start();
+		var loop = new Tone.Sequence(function(time, note) {
+			if(note) {
+				this.kickPlayer.start(time, 0, "4n", 0);
+			}
+		}.bind(this), this.state.notesToPlay, "8n");
+		loop.start();
+	}
+
+	switchNote(index, value) {
+		this.notesToPlayArr[index] = value;
+		this.setState({notesToPlay: this.notesToPlayArr});
+	}
+
+	componentDidMount() {
+		this.setState({notesToPlay: this.notesToPlayArr});
+	}
+
 	render() {
+		var noteArray = [];
+		for(var i = 0; i < 16; i++) {
+			noteArray.push(i);
+		}
+
 		return (
 			<div className="track">
-				<Note />
-				<Note />
-				<Note />
-				<Note />
-				<Note />
-				<Note />
-				<Note />
-				<Note />
-				<Note />
-				<Note />
-				<Note />
-				<Note />
-				<Note />
-				<Note />
-				<Note />
-				<Note />
+				<button onClick={this.playTrack.bind(this)}>PLAYTRACK</button>
+				{noteArray.map((x, i) => {
+					return <Note
+						 		switchNote = {this.switchNote.bind(this)}
+								id = {i}
+								key = {i}
+							 />;
+					})
+				}
 			</div>
 		);
 	}
@@ -167,8 +202,8 @@ class Track extends React.Component {
 
 class Note extends React.Component {
 
-	constructor() {
-		super();
+	constructor(props) {
+		super(props);
 		this.className = "note";
 	}
 
@@ -180,6 +215,8 @@ class Note extends React.Component {
 		var isPushed = this.state.pushed;
 		this.className = (!isPushed) ? "note-pushed" : "note";
 		this.setState({pushed: !isPushed});
+		this.props.switchNote(this.props.id, !this.state.pushed);
+		// !this.state.pushed потому что в момент вызова switchNote this.state.pushed еще не изменен
 	}
 
 	render() {
@@ -190,10 +227,15 @@ class Note extends React.Component {
 }
 
 class SequencerButtons extends React.Component {
+	constructor(props) {
+		super(props);
+		this.pushButton = this.props.play;
+	}
+
 	render() {
 		return (
 			<div className="sequencer-buttons">
-				<button>Play/stop</button>
+				<button onClick={this.pushButton}>Play/stop</button>
 				<button>Confirm</button>
 			</div>
 		);
